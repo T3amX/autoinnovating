663f20f6ad053891ada709323af1436aae913908
Zeph1rr <grianton535@gmail.com>
2022-11-02 13:36:57 +0300

    build architecture

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,32 @@
-console.log('123')
+require("dotenv").config()
+const express = require('express')
+const cors = require('cors')
+
+const appInfo = require("./package.json")
+const errorHandler = require("./middleware/errorHandling.middleware")
+const router = require('./routes')
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use("/api/v1/", router)
+
+app.use(errorHandler)
+
+
+
+const start = async () => {
+    try {
+        app.listen(PORT, () => { console.log(`${appInfo.name}:${appInfo.version} started on port ${PORT}`)})
+    } catch (e) {
+        console.log(`ERROR: ${e}`)
+    }
+}
+
+
+start().then()
+
+
